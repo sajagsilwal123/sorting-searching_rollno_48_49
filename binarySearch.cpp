cb3a3f77d6f7cc3891e065a303e9e11c97e48bf6
sajagsilwal123 <sajag.silwal123@gmail.com>
2020-02-27 04:57:02 +0545

    Binary Search

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -2,27 +2,34 @@
 using namespace std;
 #define MAX_SIZE 7
 
-void binarySearch();
-
+int binarySearch(int targ, int list[]);
 
 int main(){
-	int a[MAX_SIZE]=[1,2,8,9,10,21,98];
-	int target;
+	int a[7]={1,2,8,9,10,21,98};
+	int target,ret;
 	cout<<"Enter the data you want to search \n";
-	cin<<target;
-	binarySearch(int target, int a)
-
+	cin>>target;
+	ret = binarySearch(target, a);
+	if(ret==1)
+		cout<<target<<" Found! ";
+	else
+		cout<<target<<" Not found!";
 }
 
-
-void binarySearch(int targ, int list[]){
+int binarySearch(int targ, int list[]){
 	int min, max, mid;
 	min = 0 ;
 	max = MAX_SIZE - 1;
-	while(){
+	while(min<MAX_SIZE  && max>=0){
 		mid = (min + max)/2;
-		if(list[mid]==target)
-			cout<<targ<<"is present in the given list \n";
-		if (list)			
+		if(list[mid]==targ){
+			return 1;
+			break;
+		}			
+		else if (targ<list[mid])	
+			max = mid -1;
+		else if (targ>list[mid])
+			min = mid +	1;	 			 	
 	}
+		return -1;
 }
